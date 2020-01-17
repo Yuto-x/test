b8f4554571c0a557b69c611b7ea7ae44f047bd82
Yuto-x <57879864+Yuto-x@users.noreply.github.com>
2020-01-17 17:19:14 +0900

    Add files via upload

---
+++ ButtonHid.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHid : MonoBehaviour
+{
+    public void OnClick()
+    {
+        this.gameObject.SetActive(false);
+    }
+   
+}
