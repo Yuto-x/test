aa0790a5e434aa8addccfd1a26233b87b160290e
Yuto-x <57879864+Yuto-x@users.noreply.github.com>
2020-01-20 15:01:04 +0900

    Add files via upload

---
+++ LinqTes.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class LinqTes : MonoBehaviour
+{
+    
+    GameObject[] NullDel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        NullDel = GameObject.FindGameObjectsWithTag("Hidden");
+        foreach(GameObject tes1 in NullDel)
+        {
+            Debug.Log("削除前=" + tes1);
+        }
+        NullDel[2] = null;
+        foreach(GameObject tes in NullDel.Where(e => e != null))
+        {
+            Debug.Log("削除後=" + tes);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Tes.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tes : MonoBehaviour
+{
+    GameObject[] SecondRoomObjList; // 2階の部屋を入れる配列
+    GameObject[] SecondRoomTagList; // クリックした部屋のタグを入れる配列
+    bool Han = true;
+    private RaycastHit hit;
+    string ObjectName;
+    List<GameObject> myList = new List<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Han == true)
+        {
+            Han = false;
+            SecondRoomObjList = GameObject.FindGameObjectsWithTag("Hidden");
+            myList.Add(SecondRoomObjList[0]);
+            
+
+            Debug.Log("0番目" + myList[0]);
+            Debug.Log("0番目" + SecondRoomObjList[1]);
+            
+            
+
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                ObjectName = hit.collider.gameObject.name;
+                GameObject ObjTag = GameObject.Find(ObjectName);
+                SecondRoomTagList = GameObject.FindGameObjectsWithTag(ObjTag.tag);
+
+                foreach(GameObject hairetu in SecondRoomTagList)
+                {
+                    
+
+                    //Debug.Log("配列"+hairetu);
+                    int su = Array.IndexOf(SecondRoomObjList, hairetu);
+                    Debug.Log("数"+su);
+                }
+                
+            }
+        }
+    }
+}
